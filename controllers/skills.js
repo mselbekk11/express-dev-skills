4e64d554f6de4599b243cac5653c25d02123d304
mselbekk11 <mselbekk11@gmail.com>
2023-10-18 19:16:08 -0700

    got it working

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,6 +3,23 @@ const Skill = require('../models/skill')
 module.exports = {
   index,
   show,
+  new: newSkill,
+  create,
+  delete: deleteTodo,
+}
+
+function deleteTodo(req, res) {
+  Skill.deleteOne(req.params.id)
+  res.redirect('/skills')
+}
+
+function create(req, res) {
+  Skill.create(req.body)
+  res.redirect('/skills')
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', { title: 'New Skill' })
 }
 
 function show(req, res) {
--- routes/skills.js
+++ routes/skills.js
@@ -4,7 +4,15 @@ var skillsCtrl = require('../controllers/skills')
 
 // GET /todos
 router.get('/', skillsCtrl.index)
+// GET /todos/new <- Define BEFORE show route
+router.get('/new', skillsCtrl.new)
 //  GET / todos/:id
 router.get('/:id', skillsCtrl.show)
+
+// POST /todos
+router.post('/', skillsCtrl.create)
+
+// DELETE /todos/:id
+router.delete('/:id', skillsCtrl.delete)
 
 module.exports = router
